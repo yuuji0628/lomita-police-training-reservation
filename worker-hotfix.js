129df5cee1cbf1f6bdb1557a1aab9086af3caaa3
yuuji0628 <y6kh5p8ynw@privaterelay.appleid.com>
2026-09-20 09:49:19 +0900

    admin zip update: lomita-police-training-reservation-v2.46.zip [4/6]

--- worker-hotfix.js
+++ worker-hotfix.js
@@ -1,7 +1,7 @@
 import core from "./worker.js";
 
 /*
-  Version 2.45 retake history separation
+  Version 2.46 instructor ranking historical aggregation
 
   v2.05 の復旧取得が失敗する環境向けに、復旧経路をさらに単純化。
   - PRAGMA を使わない
@@ -19,7 +19,7 @@ const json = (data, status = 200) => new Response(JSON.stringify(data), {
   }
 });
 
-const HOTFIX_VERSION = "2.45";
+const HOTFIX_VERSION = "2.46";
 
 async function syncDisplayedVersion(response){
   try{
